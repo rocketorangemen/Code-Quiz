aa2280c191e55f9b81ff5721f360d26b487b4278
Mon <rocketorangemen@gmail.com>
2020-12-28 20:15:07 -0600

    Change Start Button/Link assests/link js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var timeleft = 60;
+var timer = setInterval(function(){
+  if(timeleft = 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Finished";
+  } else {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
