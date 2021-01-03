1e372f531e99d0757ccda5c8905035f83ccd63b7
Mon <rocketorangemen@gmail.com>
2021-01-03 08:15:48 -0600

    file changes

--- js/script.js
+++ js/script.js
@@ -8,36 +8,31 @@ var questionChoices3 = document.getElementById("questionChoices3");
 var questions = [
   {
     title: "How many images can a page contain?",
-    choices: [1,2,3],
+    choices: ["one","two","three"],
+    answer: "one"
+  },
+    {
+    title: "How many images can a page contain?",
+    choices: [11,22,33],
+    answer: 11
+  },
+    {
+    title: "How many images can a page contain?",
+    choices: [111,222,333],
     answer: 1
   },
     {
     title: "How many images can a page contain?",
-    choices: [1,2,3],
+    choices: ["run","walk","jog"],
     answer: 1
   },
-    {
-    title: "How many images can a page contain?",
-    choices: [1,2,3],
-    answer: 1
-  },
-    {
-    title: "How many images can a page contain?",
-    choices: [1,2,3],
-    answer: 1
-  },
-]
+];
 
 
 function startQuiz() {
 
-questionTitle.textContent = questions[0].title;
-questionChoices1.textContent = questions[0].choices[0];
-questionChoices2.textContent = questions[0].choices[1];
-questionChoices3.textContent = questions[0].choices[2];
-
    interval = setInterval(timer, 1000);
-
+  quizQuestions();
 }
 
 function timer (){
@@ -55,3 +50,13 @@ function timer (){
 
 };
 
+function quizQuestions() {
+  var i;
+  for (i = 0; i >= questions.length; i++) {}
+
+questionTitle.textContent = questions[0].title;
+questionChoices1.textContent = questions[i].choices[0];
+questionChoices2.textContent = questions[i].choices[1];
+questionChoices3.textContent = questions[i].choices[2];
+
+}
